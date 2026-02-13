e7ad20089f24c0e6601d5288d9caa1bbb1eabc1e
AndresY <yateloaizaandresfelipe47@gmail.com>
2026-02-13 17:09:27 -0500

    implementa suma y mayor

--- .app.py
+++ .app.py
@@ -1,8 +1,13 @@
-def analisar (lista:
-              pass  
+def analisar (lista):
+              suma=sum(lista)
+              mayor=max(lista)
+              return {
+                  "suma": suma,
+                  "mayor": mayor
+              }
   if 
   __name__==__name__:
   numeros = [4,8,2,10]
 resultado= analisar (numeros) 
 print (resultado)         
-            )
+            
