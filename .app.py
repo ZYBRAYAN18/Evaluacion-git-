61d8b4777e5a887673f46efba3e1fb4e9eebe508
martintin25 <mcrmunoz25@gmail.com>
2026-02-13 17:19:23 -0500

    inplementa menor y promedio

--- .app.py
+++ .app.py
@@ -1,8 +1,13 @@
-def analisar (lista:
-              pass  
-  if 
-  __name__==__name__:
-  numeros = [4,8,2,10]
+def analisar (lista):
+              menor = min(lista)
+              promedio=sum(lista)/len(lista)
+              return {
+                      "menor":menor,
+                      "promedio":promedio,
+              }
+if 
+__name__==__name__:
+numeros = [4,8,2,10]
 resultado= analisar (numeros) 
 print (resultado)         
-            )
+            
